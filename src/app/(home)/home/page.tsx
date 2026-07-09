1b5367367e5faac90b3aba4f2c2b0569d69dd8f3
interpago <ederjosephp7@gmail.com>
2026-07-09 12:25:57 -0500

    simplify home page for anonymous users

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -6,7 +6,6 @@ import NotFound from "@/components/sharing/NotFound";
 import Tweets from "@/components/cards/tweets/Tweets";
 import { isValidPage } from "@/lib/utils";
 import PaginationButtons from "@/components/sharing/PaginationButtons";
-import prisma from "@/lib/prismadb";
 
 interface Props {
 	searchParams: {
@@ -21,44 +20,7 @@ const Page = async ({ searchParams }: Props) => {
 	const clerkUser = await currentUser();
 
 	if (!clerkUser) {
-		try {
-			const tweets = await prisma.thread.findMany({
-				where: { parentId: null },
-				include: {
-					user: {
-						select: { id: true, imageUrl: true, name: true, username: true, followers: true, followings: true },
-					},
-					bookmarks: true,
-					likes: true,
-					_count: { select: { replies: true } },
-				},
-				orderBy: { createdAt: "desc" },
-				take: 30,
-				skip: page * 30,
-			});
-			const totalCount = await prisma.thread.count({ where: { parentId: null } });
-			return (
-				<>
-					{tweets.length ? (
-						<>
-							{tweets.map((tweet: any) => (
-								<Tweets key={tweet.id} tweet={tweet} userId="" />
-							))}
-							<PaginationButtons
-								currentPage={page}
-								currentPath={"/home"}
-								hasNext={Boolean(totalCount - page * 30 - tweets.length)}
-							/>
-						</>
-					) : (
-						<NotFound description="No posts can be displayed" />
-					)}
-				</>
-			);
-		} catch (e: any) {
-			console.error("[HOME_PAGE_ERROR]", e);
-			return <div className="text-white p-8">Error: {e?.message || "Unknown"}</div>;
-		}
+		return <div className="text-white p-8 text-xl">Hello anonymous user!</div>;
 	}
 
 	const user = await getUserAction(clerkUser.id);
