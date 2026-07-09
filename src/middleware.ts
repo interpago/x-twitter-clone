8539030970a00f875d4951f71af4f09ba7002473
interpago <ederjosephp7@gmail.com>
2026-07-09 12:20:13 -0500

    use afterAuth to allow all requests

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-	publicRoutes: ["/:path*"],
+	afterAuth(auth, req) {
+		return NextResponse.next();
+	},
 });
 
 export const config = {
