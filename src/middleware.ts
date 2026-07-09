065fc2bb9c2e27d3d3982337c189baeaac61dec2
interpago <ederjosephp7@gmail.com>
2026-07-09 12:18:21 -0500

    fix middleware with publicRoutes /:path*

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-	publicRoutes: ["/(.*)"],
-	ignoredRoutes: ["/(.*)"],
+	publicRoutes: ["/:path*"],
 });
 
 export const config = {
-	matcher: [],
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
